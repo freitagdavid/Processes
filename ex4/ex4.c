c4cbca6722a98845b7ccdda3d64b893cd1d787a6
David Freitag <freitagdavid@outlook.com>
2019-05-01 16:58:59 -0400

    ex4/ex4: Complete

--- ex4/ex4.c
+++ ex4/ex4.c
@@ -1,16 +1,24 @@
 // Write a program that calls `fork()` and then calls some form of `exec()`
-// to run the program `/bin/ls`. Try a few variants of `exec()`, such as 
-// `execl()`, `execle()`, `execv()`, and others. Why do you think there 
+// to run the program `/bin/ls`. Try a few variants of `exec()`, such as
+// `execl()`, `execle()`, `execv()`, and others. Why do you think there
 // are so many variants of the same basic call?
 
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+#include <unistd.h>
 
-int main(void)
-{
-    // Your code here    
-
-    return 0;
+int main(void) {
+  // Your code here
+  int rc = fork();
+  if (rc == 0) {
+    char *lsargs[3];
+    lsargs[0] = "/bin/ls";
+    lsargs[1] = "./";
+    lsargs[2] = NULL;
+    execvp(lsargs[0], lsargs);
+  } else if (rc > 0) {
+    printf("Just look at that concurrency!\n");
+  }
+  return 0;
 }
