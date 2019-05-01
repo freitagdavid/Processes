984e7d7db5b37313f077bac1642147e2ddc5f7f3
David Freitag <freitagdavid@outlook.com>
2019-05-01 16:42:59 -0400

    ex2/ex2: Complete

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -1,14 +1,44 @@
-// Write a program that opens the text.txt file (with the `fopen()` library call) located in this directory 
-// and then calls `fork()` to create a new process. Can both the child and parent access the file descriptor 
-// returned by `fopen()`? What happens when they are written to the file concurrently?
+// Write a program that opens the text.txt file (with the `fopen()` library
+// call) located in this directory and then calls `fork()` to create a new
+// process. Can both the child and parent access the file descriptor returned by
+// `fopen()`? What happens when they are written to the file concurrently?
 
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-int main(void)
-{
-    // Your code here 
-    
-    return 0;
+void *printFile(char *filename) {
+  FILE *file = fopen(filename, "r");
+  int c;
+  while (1) {
+    c = fgetc(file);
+    if (feof(file)) {
+      break;
+    }
+    printf("%c", c);
+  }
+  fclose(file);
 }
+
+void *writeFile(char *filename, char *input) {
+  FILE *file = fopen(filename, "w");
+  fputs(input, file);
+  fclose(file);
+}
+
+int main(void) {
+  writeFile("text.txt", "Parent Process: Print to file test 1\n");
+  printFile("text.txt");
+  int rc = fork();
+  if (rc == 0) {
+    writeFile("text.txt", "WAIT: Parent Process: Print to file after fork.\n");
+  }
+  if (rc > 0) {
+    writeFile("text.txt", "WAIT: Child Process: Print to file after fork.\n");
+  }
+  printFile("text.txt");
+  return 0;
+}
+
+// Inconsistent writes when done simultaneously sometimes child writes sometimes
+// parent writes.
