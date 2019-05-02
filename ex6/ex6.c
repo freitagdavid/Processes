f466d8d49e47df0ed964d1af79935030000cb1f9
David Freitag <freitagdavid@outlook.com>
2019-05-02 16:01:36 -0400

    ex6/ex6: Complete

--- ex6/ex6.c
+++ ex6/ex6.c
@@ -1,26 +1,41 @@
 /*
 Write a program that will find the average time it takes for your computer
-to make a system call an empty write to stdout. Your program should use the 
-`clock_gettime` procedure to time how long a single system call takes. It'll 
-do this for one million iterations, and then find the average of all of those 
+to make a system call an empty write to stdout. Your program should use the
+`clock_gettime` procedure to time how long a single system call takes. It'll
+do this for one million iterations, and then find the average of all of those
 iterations in nanoseconds.
 For some helpful documentation and examples of using time-related system calls,
-visit this site: https://www.cs.rutgers.edu/~pxk/416/notes/c-tutorials/gettime.html
-While the linked site does say that `clock_gettime()` does not work on OSX, this 
-turns out to only be the case for OSX versions < 10.12. Anything later than that 
-and `clock_gettime()` should work just fine. 
+visit this site:
+https://www.cs.rutgers.edu/~pxk/416/notes/c-tutorials/gettime.html While the
+linked site does say that `clock_gettime()` does not work on OSX, this turns out
+to only be the case for OSX versions < 10.12. Anything later than that and
+`clock_gettime()` should work just fine.
 */
 
 #include <stdio.h>
-#include <unistd.h>
 #include <time.h>
+#include <unistd.h>
 
 #define number_iter 1000000
 #define BILLION 1000000000L
 
-int main()
-{
-    // Your code here
-    
-    return 0;
+int main() {
+  // Your code here
+  struct timespec start, end;
+  int total_run_time;
+  int average_runtime;
+
+  for (int i = 0; i < number_iter; i++) {
+    clock_gettime(CLOCK_MONOTONIC, &start);
+
+    printf("");
+
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    total_run_time += (int)BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec -
+                      start.tv_nsec;
+  }
+  average_runtime = total_run_time / number_iter;
+  printf("%i\n", average_runtime);
+  printf("Done\n");
+  return 0;
 }
